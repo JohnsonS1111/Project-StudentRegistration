961c293302f52127715e02e1ba1fd45526a2a2c8
Johnson Shogbaike <g00389668@atu.ie>
2023-12-22 15:09:27 +0000

    Created new microservice for student registration system called StudentRegistration

---
+++ src/test/java/ie/atu/projectstudentregistration/ProjectStudentRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.projectstudentregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStudentRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
