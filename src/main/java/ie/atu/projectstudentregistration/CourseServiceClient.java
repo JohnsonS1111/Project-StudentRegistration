f27ddf4b80dd3e1bbcc97bfe82e68370207084f6
Johnson Shogbaike <g00389668@atu.ie>
2024-01-11 14:39:20 +0000

    changes to the controller

--- src/main/java/ie/atu/projectstudentregistration/CourseServiceClient.java
+++ src/main/java/ie/atu/projectstudentregistration/CourseServiceClient.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "payment-service", url = "http://localhost:8082")
-public interface PaymentServiceClient {
+@FeignClient(name = "course-service", url = "http://localhost:8082")
+public interface CourseServiceClient {
     @PostMapping("/payment")
     String studentDetails(@RequestBody StudentClass studentClass);
 }
--- src/main/java/ie/atu/projectstudentregistration/StudentClass.java
+++ src/main/java/ie/atu/projectstudentregistration/StudentClass.java
@@ -1,6 +1,9 @@
 package ie.atu.projectstudentregistration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,16 +13,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class StudentClass {
+
     @JsonProperty("firstname")
+    @NotBlank(message = "firstname cannot be blank")
     private String firstname;
 
     @JsonProperty("surname")
+    @NotBlank(message = "surname cannot be blank")
     private String surname;
 
-    @JsonProperty("birthdate")
+    @JsonProperty("age")
+    @Min(value = 17, message = "user must be 17 or older to register")
     private int age;
 
     @JsonProperty("email")
+    @Email(message = "invalid email address")
+    @NotBlank(message = "email cannot be blank")
     private String email;
 
 }
